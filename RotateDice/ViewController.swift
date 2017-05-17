5f7c9a858949387bdbf869206c846b93f25b7a76
Simon Ng <simon@simons-macbook-pro-2.local>
2017-05-17 16:07:05 +0800

    Initial Commit

---
+++ RotateDice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RotateDice
+//
+//  Created by Simon Ng on 17/5/2017.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
